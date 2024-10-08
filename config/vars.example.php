267a2f46f0411d276bf0d2e2390b026fe677ecf2
Abd Whb <winnerk088@gmail.com>
2024-10-08 18:14:25 +0100

    Change config/vars to config/vars.example

---
+++ config/vars.example.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'join_code' => '', // Put a registration code for invited staff member here.
+    'roles' => [
+        'admin',
+        'superadmin',
+        'root',
+    ],
+    'admin_route_prefix' => 'portal.',
+    'menu_sections' => [
+        'conservation',
+        'our-story',
+        'traveler-resources',
+        'more',
+    ]
+];
